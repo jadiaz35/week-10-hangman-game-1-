12761ff334dea275e1c9d7dae025402b14bcdf07
Juan Diaz <113930348+jadiaz35@users.noreply.github.com>
2022-10-27 17:52:51 +0000

    half way through return

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 #from methods_help import method_help
-from functions import welcome, add_numbers, num_dividedFrom_add_numbers, greet1, square
-# from returnStuff import returnS
+# from functions import welcome, add_numbers, num_dividedFrom_add_numbers, greet1, square
+from returnStuff import returnS, power, usd_to_eur
 
 #method_help()
 # function()
-welcome("Bob")
-added = add_numbers(4,5)
-print(added)
-final = num_dividedFrom_add_numbers(45, added)
-print(final)
-greet1()
-squared = square(5)
-print(squared)
-# returnS()
+# welcome("Bob")
+# added = add_numbers(4,5)
+# print(added)
+# final = num_dividedFrom_add_numbers(45, added)
+# print(final)
+# greet1()
+# squared = square(5)
+# print(squared)
+returnS()
+exponents = power(6,2)
+print(exponents)
+euro = usd_to_eur(10)
+print(euro)
--- returnStuff.py
+++ returnStuff.py
@@ -7,7 +7,8 @@ def returnS():
   # Create a function called power that takes two numeric values as arguments. It must return the number that results from solving a power, 
   # using the first number as the base, and the second as the exponent:
 
-
+def power(num1,num2):
+  return num1**num2
 
 
 
@@ -20,7 +21,9 @@ def returnS():
 
 # Hint: to perform the conversion, the function internally must multiply this value in dollars by 0.90 to obtain the equivalent amount in euros.
 
-
+def usd_to_eur(usd):
+  return usd*0.9
+  
 
 
 
@@ -35,3 +38,5 @@ def returnS():
 # Also, you must create a variable called word, which contains any string, to pass it as an argument to the created function.
 
 # Hint: inside the created function, you should use string methods already seen.
+
+def reverse_word

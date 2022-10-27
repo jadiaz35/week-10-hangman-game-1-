5bfc86f13935588ef8af0ecbfb285f098942b1cc
Juan Diaz <113930348+jadiaz35@users.noreply.github.com>
2022-10-27 17:22:11 +0000

    divided numbers

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from methods_help import method_help
-from functions import function
-from returnStuff import returnS
+#from methods_help import method_help
+from functions import function, welcome, add_numbers, num_dividedFrom_add_numbers
+# from returnStuff import returnS
 
-method_help()
+#method_help()
 function()
-returnS()
+welcome("Yahaira")
+added = add_numbers(4,5)
+print(added)
+final = num_dividedFrom_add_numbers(45, added)
+print(final)
+# returnS()
